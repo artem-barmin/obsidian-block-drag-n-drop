b8f11c34ded910f0ad12f9698b433c2c155b056a
Artem Barmin <artem.barmin@gmail.com>
2022-03-29 00:43:54 +0300

    Initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,87 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView } from "obsidian";
+import { gutter, GutterMarker } from "@codemirror/gutter";
+import { EditorView } from "@codemirror/view";
+import { LoggerService } from "obsidian-outliner/src/services/LoggerService";
+import { ParserService } from "obsidian-outliner/src/services/ParserService";
+import { ApplyChangesService } from "obsidian-outliner/src/services/ApplyChangesService";
+import { PerformOperationService } from "obsidian-outliner/src/services/PerformOperationService";
+import { MyEditor } from "obsidian-outliner/src/MyEditor";
 
-// Remember to rename these classes and interfaces!
+const emptyMarker = (line) =>
+	new (class extends GutterMarker {
+		toDOM() {
+			const handle = document.createElement("div");
+			handle.appendChild(document.createTextNode(":::"));
+			handle.className = "gutter-marker";
+			handle.setAttribute("draggable", true);
+			handle.addEventListener("dragstart", (e) =>
+				e.dataTransfer.setData("line", line)
+			);
+			return handle;
+		}
+	})();
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const emptyLineGutter = gutter({
+	lineMarker(view, line) {
+		return line.from == line.to
+			? null
+			: emptyMarker(view.state.doc.lineAt(line.from).number);
+	},
+});
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
+		this.logger = new LoggerService({});
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.parser = new ParserService(this.logger);
+		this.applyChanges = new ApplyChangesService();
+		const performOperation = new PerformOperationService(
+			this.parser,
+			this.applyChanges
+		);
+		const that = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerEditorExtension(emptyLineGutter);
+		this.registerEditorExtension(
+			EditorView.domEventHandlers({
+				dragover(event, view) {
+					// add class to element
+					const target = event.target as HTMLElement;
+					if (target.classList.contains("HyperMD-list-line")) {
+						target.classList.add("drag-over");
+						event.preventDefault();
 					}
+				},
+				dragleave(event, view) {
+					// remove class from element
+					const target = event.target as HTMLElement;
+					target.classList.remove("drag-over");
+				},
+				drop(event, editor) {
+					const sourceLine = event.dataTransfer.getData("line");
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+					const view =
+						that.app.workspace.getActiveViewOfType(MarkdownView);
+					if (view && view.editor) {
+						const editor = view.editor;
+						console.log(">>>>", event);
+						performOperation.performOperation(
+							(root) => ({
+								shouldUpdate: () => true,
+								shouldStopPropagation: () => false,
+								perform: () => {
+									if (!root.hasSingleCursor()) return;
+									const sourceList =
+										root.getListUnderLine(sourceLine);
+									console.log(sourceLine);
+								},
+							}),
+							new MyEditor(editor),
+							editor.getCursor()
+						);
+					}
+				},
+			})
+		);
 	}
 }
